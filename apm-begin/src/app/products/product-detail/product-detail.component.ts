440d2aa86b9a73f6fbf871d42e26e5c7bf3b61af
BGarmyn <benjamin_garmyn@hotmail.com>
2024-06-02 12:07:42 +0200

    update

--- apm-begin/src/app/products/product-detail/product-detail.component.ts
+++ apm-begin/src/app/products/product-detail/product-detail.component.ts
@@ -1,25 +1,60 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  SimpleChanges,
+  inject,
+} from '@angular/core';
 
 import { NgIf, NgFor, CurrencyPipe } from '@angular/common';
 import { Product } from '../product';
+import { ProductService } from '../product.service';
+import { EMPTY, Subscription, catchError, tap } from 'rxjs';
 
 @Component({
-    selector: 'pm-product-detail',
-    templateUrl: './product-detail.component.html',
-    standalone: true,
-    imports: [NgIf, NgFor, CurrencyPipe]
+  selector: 'pm-product-detail',
+  templateUrl: './product-detail.component.html',
+  standalone: true,
+  imports: [NgIf, NgFor, CurrencyPipe],
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnChanges, OnDestroy {
   // Just enough here for the template to compile
   @Input() productId: number = 0;
   errorMessage = '';
+  sub!: Subscription;
+
+  private productService = inject(ProductService);
 
   // Product to display
   product: Product | null = null;
 
   // Set the page title
-  pageTitle = this.product ? `Product Detail for: ${this.product.productName}` : 'Product Detail';
+  pageTitle = this.product
+    ? `Product Detail for: ${this.product.productName}`
+    : 'Product Detail';
 
-  addToCart(product: Product) {
+  ngOnChanges(changes: SimpleChanges): void {
+    const id = changes['productId'].currentValue;
+    if (id) {
+      this.sub = this.productService
+        .getProduct(id)
+        .pipe(
+          tap((product) => (this.product = product)),
+          catchError((err) => {
+            this.errorMessage = err;
+            return EMPTY;
+          })
+        )
+        .subscribe();
+    }
   }
+
+  ngOnDestroy(): void {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
+
+  addToCart(product: Product) {}
 }
--- apm-begin/src/app/products/product-list/product-list.component.ts
+++ apm-begin/src/app/products/product-list/product-list.component.ts
@@ -1,25 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 
 import { NgIf, NgFor, NgClass } from '@angular/common';
 import { Product } from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { ProductService } from '../product.service';
+import { EMPTY, Subscription, catchError, tap } from 'rxjs';
 
 @Component({
-    selector: 'pm-product-list',
-    templateUrl: './product-list.component.html',
-    standalone: true,
-  imports: [NgIf, NgFor, NgClass, ProductDetailComponent]
+  selector: 'pm-product-list',
+  templateUrl: './product-list.component.html',
+  standalone: true,
+  imports: [NgIf, NgFor, NgClass, ProductDetailComponent],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit, OnDestroy {
   // Just enough here for the template to compile
   pageTitle = 'Products';
   errorMessage = '';
+  sub!: Subscription;
+
+  private productService = inject(ProductService);
 
   // Products
   products: Product[] = [];
 
   // Selected product id to highlight the entry
   selectedProductId: number = 0;
+
+  ngOnInit(): void {
+    this.sub = this.productService
+      .getProducts()
+      .pipe(
+        tap((products) => {
+          console.log('In component pipeline');
+          this.products = products;
+        }),
+        catchError((err) => {
+          this.errorMessage = err;
+          return EMPTY;
+        })
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
   onSelected(productId: number): void {
     this.selectedProductId = productId;
